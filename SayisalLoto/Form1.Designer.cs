b0a75ca2d0f8f1b49c56ccffa7d5bc29faa5ccdf
Hanife Cennet Alkan <hanifecennetalkan@gmail.com>
2022-09-07 15:26:43 +0300

    SayısalLoto

---
+++ SayisalLoto/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SayisalLoto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.FHFHFGH = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(215, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "KURA ÇEK";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGreen;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(325, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "SIRALA";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(223, 92);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(169, 184);
+            this.listBox1.TabIndex = 2;
+            // 
+            // FHFHFGH
+            // 
+            this.FHFHFGH.AutoSize = true;
+            this.FHFHFGH.BackColor = System.Drawing.Color.Transparent;
+            this.FHFHFGH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FHFHFGH.Location = new System.Drawing.Point(211, 55);
+            this.FHFHFGH.Name = "FHFHFGH";
+            this.FHFHFGH.Size = new System.Drawing.Size(200, 20);
+            this.FHFHFGH.TabIndex = 3;
+            this.FHFHFGH.Text = "ÇEKİLEN ŞANSLI SAYI";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SayisalLoto.Properties.Resources.loto;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FHFHFGH);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SAYISAL LOTO";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label FHFHFGH;
+    }
+}
+
---
+++ SayisalLoto/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayisalLoto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            int[] loto = new int[6];
+
+            Random r = new Random();
+            loto[0] = Convert.ToInt32(Convert.ToInt32(r.Next(49) + 1));
+            listBox1.Items.Add(loto[0]); //yazdırmak için
+
+            for (int i= 1;  i<=5 ; i++)
+            {
+                loto[i] = Convert.ToInt32(Convert.ToInt32(r.Next(49) + 1));
+                for (int j = 0; j <=(i-1); j++)
+                {
+                    if (loto[i]==loto[j]) //eşitse kuranın tekrar çekilmesi sağlanacak
+                    {
+                        loto[i] = Convert.ToInt32(Convert.ToInt32(r.Next(49) + 1));
+                        j = j - 1;
+                    }
+                }
+
+                listBox1.Items.Add(loto[i]); //her defasında değilde işlem bitince yazdırırız daha basit
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int k = 0; k < listBox1.Items.Count; k++)
+
+                //her bir değeri için
+                for (int l = k + 1; l < listBox1.Items.Count; l++)
+                {
+                    if (Convert.ToInt32(listBox1.Items[k].ToString()) > Convert.ToInt32(listBox1.Items[l].ToString()))
+                    {
+                        int degis = Convert.ToInt32(listBox1.Items[k].ToString());
+                        listBox1.Items[k] = listBox1.Items[l].ToString();
+                        listBox1.Items[l] = degis;
+                    }
+                }
+            
+        }
+    }}
+
+
